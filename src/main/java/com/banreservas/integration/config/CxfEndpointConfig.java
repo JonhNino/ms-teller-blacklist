83cdeb4ded9ac019a936f623dfc04307818379c0
JonhNino <jonhninoa@gmail.com>
2024-11-19 21:11:15 -0500

    19/11/24 Validacion rq vacio

--- src/main/java/com/banreservas/integration/config/CxfEndpointConfig.java
+++ src/main/java/com/banreservas/integration/config/CxfEndpointConfig.java
@@ -1,38 +1,26 @@
 package com.banreservas.integration.config;
 
+import com.banreservas.integration.handler.SoapMessageHandler;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.cxf.common.DataFormat;
 import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
+import org.apache.cxf.feature.LoggingFeature;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * Clase de configuración que produce y configura un endpoint CXF para servicios
- * SOAP en Apache Camel.
- *
- * @author Angel Gonzalez
- * @since 21/10/2024
- * @version 1.0
- */
 @ApplicationScoped
 public class CxfEndpointConfig {
 
     @ConfigProperty(name = "blacklist.service.soap.address")
     private String address;
 
-    /**
-     * Este método es utilizado para generar un endpoint de CXF que se configura con
-     * una dirección (establecida en el campo {@code address}), un formato de datos {@link DataFormat#PAYLOAD}
-     * y habilita las características necesarias en el endpoint, incluyendo el manejo de cuerpos vacíos.
-     * El {@link CamelContext} proporcionado se asocia con el endpoint para gestionar
-     * el ciclo de vida del mismo dentro de la aplicación Camel.
-     *
-     * @param camelContext El contexto de Camel que será asociado al endpoint.
-     * @return El objeto {@link CxfEndpoint} configurado y listo para su uso.
-     */
     @Produces
     @ApplicationScoped
     @Named("cxfEndpoint")
@@ -41,7 +29,37 @@ public class CxfEndpointConfig {
         cxfEndpoint.setAddress(address);
         cxfEndpoint.setCamelContext(camelContext);
         cxfEndpoint.setDataFormat(DataFormat.PAYLOAD);
-        cxfEndpoint.setLoggingFeatureEnabled(true);
+
+        configureEndpointProperties(cxfEndpoint);
+        configureInterceptors(cxfEndpoint);
+        configureLogging(cxfEndpoint);
+
         return cxfEndpoint;
+    }
+
+    private void configureEndpointProperties(CxfEndpoint cxfEndpoint) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("schema-validation-enabled", true);
+        properties.put("exceptionMessageCauseEnabled", true);
+        properties.put("allowEmptyInput", false);
+        properties.put("validationEnabled", true);
+        cxfEndpoint.setProperties(properties);
+    }
+
+    private void configureInterceptors(CxfEndpoint cxfEndpoint) {
+        cxfEndpoint.getInInterceptors().add(
+                new AbstractPhaseInterceptor<Message>(Phase.RECEIVE) {
+                    @Override
+                    public void handleMessage(Message message) {
+                        SoapMessageHandler.validateMessage(message);
+                    }
+                }
+        );
+    }
+
+    private void configureLogging(CxfEndpoint cxfEndpoint) {
+        LoggingFeature loggingFeature = new LoggingFeature();
+        loggingFeature.setPrettyLogging(true);
+        cxfEndpoint.getFeatures().add(loggingFeature);
     }
 }
--- src/main/java/com/banreservas/integration/routes/BlackListVerificationRoute.java
+++ src/main/java/com/banreservas/integration/routes/BlackListVerificationRoute.java
@@ -1,9 +1,9 @@
 package com.banreservas.integration.routes;
 
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
 import org.jboss.logging.Logger;
 
 @ApplicationScoped
@@ -12,12 +12,11 @@ public class BlackListVerificationRoute extends RouteBuilder {
     @Inject
     CxfEndpoint cxfEndpoint;
 
-
     private static final Logger LOGGER = Logger.getLogger(BlackListVerificationRoute.class);
 
     @Override
     public void configure() throws Exception {
-        // No necesitamos manejar excepciones aquí ya que el interceptor se encarga
+        // Configuración de la ruta principal
         from(cxfEndpoint)
                 .routeId("SoapServiceRoute")
                 .log("Procesando solicitud SOAP")
